e4d97d5907a562c2d3840b2c712e6c7d1e0a06c3
takuya178 <takuya.u1005@gmail.com>
2024-04-26 16:29:09 +0900

    add mse

--- LinerRegression/script_test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def cost_func(theta_0: int, theta_1: int, x: list, y: list):
-  return np.mean(np.square(y - (theta_0 + theta_1 * x)))
-
-def test(theta_0: int, theta_1: int, x: list, y: list):
-  return np.square(y - (theta_0 + theta_1 * x))
